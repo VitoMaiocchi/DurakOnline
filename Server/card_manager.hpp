3251dfcabd5cdf768ccc5cfe12b0a348712289c5
Vito Maiocchi <vito.maiocchi@outlook.com>
2024-10-30 11:33:12 +0100

    include fix

--- Server/card_manager.hpp
+++ Server/card_manager.hpp
@@ -1,15 +1,15 @@
 #ifndef CARD_MANAGER_HPP
 #define CARD_MANAGER_HPP
 
-#include "../Networking/include/Networking/util.hpp"
 #include <vector>
 #include <deque>
+#include <Networking/util.hpp>
 
 class card_manager
 {
 private:
     std::deque<Card> deck;              //represents cards in the middle
-    *Card last_card;                    //Pointer to the last card in the deck, maybe make it const
+    Card* last_card;                    //Pointer to the last card in the deck, maybe make it const
     unsigned int number_cards_in_deck;  //Number of cards in the deck
     bool endgame;                       //Could be useful for endgame functions, delete if it is not used
     std::vector<Card> discarded_cards;  //Charte wo "weg" sind
@@ -34,7 +34,7 @@ public:
     std::vector<unsigned int> getPlayerHand (unsigned int PlayerID);
     std::vector<std::pair<Card,Card>> getMiddle();
     unsigned int getNumberActivePlayers();
-    unsigned int getNumberOfCardsInHand(unsigned int PlayerID)
+    unsigned int getNumberOfCardsInHand(unsigned int PlayerID);
 
     // game actions
     bool playCard(Card card, unsigned int PlayerID);   //Playing a card to attack or defend, I might have to add some arguments like the position where the card will be played or if the player is an attacker or a defender 

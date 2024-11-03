6cfd960f1a07a42f7f028cf34cf76b0c2657681a
damcha02 <danil.poluyanov13@gmail.com>
2024-11-03 21:17:01 +0100

    added code skeleton in battle.cpp

--- Server/battle.cpp
+++ Server/battle.cpp
@@ -1,7 +1,26 @@
 #include "battle.hpp"
-
+#include <iostream>
 
 bool Battle::handleCardEvent(){
     return false;
 }
 
+bool Battle::handleActionEvent(){
+    return false;
+}
+
+bool Battle::successfulDefend(){
+    return false;
+}
+
+bool Battle::passOn(){
+    return false;
+}
+
+bool Battle::isValidMove(){
+    return false;
+}
+
+void Battle::attack(){}
+
+void Battle::defend(){}

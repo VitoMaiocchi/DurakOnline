545574c6ff35166852f9843273de604eca7a9fe4
damcha02 <danil.poluyanov13@gmail.com>
2024-11-06 13:57:09 +0100

    game.hpp and .cpp skeleton

--- Server/include/game.hpp
+++ Server/include/game.hpp
@@ -1,1 +1,28 @@
-class Game;
+#ifndef GAME_HPP
+#define GAME_HPP
+#include "card_manager.hpp"
+#include "battle.hpp"
+#include <vector>
+#include <tuple>
+
+using player_id = unsigned int;
+
+class Game{
+    private:
+        std::vector<std::pair<int, /*enum player role*/>> players_bs; //attacking, defending, spectating
+        Battle* current_battle;
+        CardManager card_manager;
+    public:
+        bool createGame();
+        bool makeFirstBattle();
+        bool createBattle();
+        bool isStarted();
+        bool endGame();
+        bool resetGame();
+        bool updateTurnOrder();
+        bool handleClientActionEvent();
+        bool handleClientCardEvent();
+};
+
+
+#endif
---
+++ Server/src/game.cpp
@@ -0,0 +1,41 @@
+#include "../include/game.hpp"
+
+#include <iostream>
+
+
+
+bool Game::createGame(){
+    return false;
+}
+
+bool Game::makeFirstBattle(){
+    return false;
+}
+
+bool Game::createBattle(){
+    return false;
+}
+
+bool Game::isStarted(){
+    return false;
+}
+
+bool Game::endGame(){
+    return false;
+}
+
+bool Game::resetGame(){
+    return false;
+}
+
+bool Game::updateTurnOrder(){
+    return false;
+}
+
+bool Game::handleClientActionEvent(){
+    return false;
+}
+
+bool Game::handleClientCardEvent(){
+    return false;
+}

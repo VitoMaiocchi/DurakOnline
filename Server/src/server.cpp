72381f5220ba8b2b4b67d38cbfe30094efc399ab
damcha02 <danil.poluyanov13@gmail.com>
2024-11-06 22:22:04 +0100

    added a message distribution skeleton to server

--- Server/src/server.cpp
+++ Server/src/server.cpp
@@ -25,11 +25,43 @@ void broadcastMessage(std::unique_ptr<Message> message) {
 void handleMessage(std::unique_ptr<Message> message, ClientID client) {
     //da message handle dies das
     switch (message->messageType) {
-        MESSAGETYPE_CARD_UPDATE:
+        case MESSAGETYPE_TEST:
+            std::cout << "just a test message"<<std::endl;
+        break;
+        case MESSAGETYPE_ILLEGAL_MOVE_NOTIFY:
+            //something
+        break;
+        case MESSAGETYPE_CARD_UPDATE:
             //do something
             //Network::sendMessage(client, goo goo gagag);
         break;
-        //...
+        case MESSAGETYPE_PLAYER_UPDATE:
+            //something
+        break;
+        case MESSAGETYPE_BATTLE_STATE_UPDATE:
+            //something
+        break;
+        case MESSAGETYPE_AVAILABLE_ACTION_UPDATE:
+            //something
+        break;
+        case MESSAGETYPE_GAME_STATE_UPDATE:
+            //something
+        break;
+        case MESSAGETYPE_PLAYCARD_EVENT:
+            //something
+        break;
+        case MESSAGETYPE_CLIENT_ACTION_EVENT:
+            //something
+        break;
+        case MESSAGETYPE_CLIENT_CONNECT_EVENT:
+            //something
+        break;
+        case MESSAGETYPE_CLIENT_DISCONNECT_EVENT:
+            //something
+        break;
+        default:
+            std::cout << "messagetype not found" << std::endl;
+        break;
     }
 }
 

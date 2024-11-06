2264372ac898b5561b54e5d381e46bd9f2826561
damcha02 <danil.poluyanov13@gmail.com>
2024-11-06 22:02:11 +0100

    crucial changes, now server can connect to client now we can test the server interactions

--- Server/src/server.cpp
+++ Server/src/server.cpp
@@ -1,7 +1,9 @@
 #define NETWORKTYPE_SERVER
 #include <Networking/network.hpp>
-#include "server.hpp"
-#include "game.hpp"
+#include <Networking/message.hpp>
+
+#include "../include/server.hpp"
+#include "../include/game.hpp"
 #include <unordered_set>
 #include <iostream>
 
@@ -34,45 +36,45 @@ void handleMessage(std::unique_ptr<Message> message, ClientID client) {
 int main() {
 
     //start networking
-    // Network::openSocket(42069);
-    // //set up irgend welches züg etc
-
-    // while(true) {
-    //     //listen for messages
-    //     ClientID client;
-    //     handleMessage(Network::reciveMessage(client), client);
-    // }
-    return 0;
-
-}
-
-int main() {
     Network::openSocket(42069);
-    std::unordered_set<ClientID> clients;
-    while(true) {
-        ClientID id;
-        std::unique_ptr<Message> m = Network::reciveMessage(id);
-        if(m == nullptr){
-            std::cerr << "null ptr received" <<std::endl;
-            return -1;
-        }
-        else{
-            std::cout << "message received" << std::endl;
-        }
-        clients.insert(id);
-        switch (m->messageType) {
-            case MESSAGETYPE_TEST:
-                dynamic_cast<TestMessage*>(m.get())->x = id;
-                for(auto client : clients) {
-                    Network::sendMessage(m, client);
-                    std::cout << "message sending with type: " << m->messageType << std::endl;
-                }
-            break;
-            case MESSAGETYPE_CLIENT_DISCONNECT_EVENT:
-                std::cout << "CLIENT DISCONNECTED: "<<id << std::endl;
-            break;
-        }
-    }
+    //set up irgend welches züg etc
 
+    while(true) {
+        //listen for messages
+        ClientID client;
+        handleMessage(Network::reciveMessage(client), client);
+    }
     return 0;
+
 }
+
+// int main() {
+//     Network::openSocket(42069);
+//     std::unordered_set<ClientID> clients;
+//     while(true) {
+//         ClientID id;
+//         std::unique_ptr<Message> m = Network::reciveMessage(id);
+//         if(m == nullptr){
+//             std::cerr << "null ptr received" <<std::endl;
+//             return -1;
+//         }
+//         else{
+//             std::cout << "message received" << std::endl;
+//         }
+//         clients.insert(id);
+//         switch (m->messageType) {
+//             case MESSAGETYPE_TEST:
+//                 dynamic_cast<TestMessage*>(m.get())->x = id;
+//                 for(auto client : clients) {
+//                     Network::sendMessage(m, client);
+//                     std::cout << "message sending with type: " << m->messageType << std::endl;
+//                 }
+//             break;
+//             case MESSAGETYPE_CLIENT_DISCONNECT_EVENT:
+//                 std::cout << "CLIENT DISCONNECTED: "<<id << std::endl;
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }

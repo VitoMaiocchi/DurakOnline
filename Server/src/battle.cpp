9376cd5214454996dbc01a68a17cb60f143f1f81
damcha02 <danil.poluyanov13@gmail.com>
2024-11-08 13:32:27 +0100

    changed battle.hpp and util.hpp

--- Server/src/battle.cpp
+++ Server/src/battle.cpp
@@ -6,19 +6,25 @@
 * QUESTIONS: 
  */
 
-//constructor 
-Battle::Battle(bool first_battle) : first_battle_(first_battle){
+//constructor, passes if it is first battle or not and passes the players with their roles
+Battle::Battle(bool first_battle, std::vector<std::pair<int, PlayerRole>> players) : 
+                                    first_battle_(first_battle), players_bs(players), curr_attacks(0){
     if(first_battle_ == true){
         max_attacks = 5;
     }
     else{
         max_attacks = 6;
     }
-}
-bool Battle::handleCardEvent(/* the message card event */){
+};
+
+bool Battle::handleCardEvent(/* the message play card event */){
 
     std::cout << "handleCardEvent was called" << std::endl;
 
+    //calls isvalidmove and gives the message parameters with the function,
+    //here we should handle the list and maybe break it down? by card, then we can call isValidMove multiple 
+    //times and check if each card is valid, and if yes, then we give to go
+    
     return false;
 }
 
@@ -42,8 +48,6 @@ bool Battle::passOn(/*unsigned player_id*/){
     //getPlayerHand(player_id) check if a card with the same number is in the hand
     //if no return invalid move
     //if yes set attacker to idle, defender to attacker, coattacker to defender, the next player to coattacker
-
-    
     return false;
 }
 
@@ -56,7 +60,16 @@ bool Battle::passOn(/*unsigned player_id*/){
                 being played? 
 
  */ 
-bool Battle::isValidMove( /*const Card &card, int player_id*/){
+//informs server that a player is trying to play a card -> specific slot?
+// struct PlayCardEvent : public Message {
+//     PlayCardEvent();
+//     void getContent(rapidjson::Value &content, Allocator &allocator) const;
+//     void fromJson(const rapidjson::Value& obj);
+
+//     std::list<Card> cards; // can be multiple if multiple cards are played at once, max 4
+//     CardSlot slot; //place of the card
+// };
+bool Battle::isValidMove( const Card &card, int player_id /*message with slots?*/){
 
     //if its a defender -> how to check that?
     //call cards in middle 
@@ -68,6 +81,31 @@ bool Battle::isValidMove( /*const Card &card, int player_id*/){
     //if yes then true
     //else if its two or more cards at the same time check that all of the cards are the same number
     //else if the card is the same number as one of the cards in the middle then it is ok to play
+
+    //set the role per default to idle and it will return false if this is not changed
+    int role = IDLE;
+
+    //get the role of the player that is trying to play the card
+    for(auto& player : players_bs ){
+        if(player.first == player_id){
+            role = player.second;
+            break;
+        }
+    }
+    if(role == DEFENDER){
+        
+    }
+    if(role == ATTACKER){
+        if(curr_attacks == max_attacks){
+            return false;
+        }
+        if(curr_attacks == 0 /*&& message -> */){
+            return true;
+        }
+    }
+    if(role == IDLE){
+        return false;
+    }
 
     return false;
 }

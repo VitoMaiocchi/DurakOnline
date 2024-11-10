8cb5041a5106c252d2f0885baa1c701c93d7d855
damcha02 <danil.poluyanov13@gmail.com>
2024-11-11 00:51:18 +0100

    added a new helper function to manipulate msg frmt

--- Server/src/msg_handler.cpp
+++ Server/src/msg_handler.cpp
@@ -1,7 +1,7 @@
 #include "../include/msg_handler.hpp"
 #include <iostream>
 
-void handleMessage(std::unique_ptr<Message> message, ClientID client){
+void handleMessage(std::unique_ptr<Message> message, ClientID client, Game* current_game){
     //da message handle dies das
     switch (message->messageType) {
         case MESSAGETYPE_TEST:
@@ -28,6 +28,10 @@ void handleMessage(std::unique_ptr<Message> message, ClientID client){
         break;
         case MESSAGETYPE_PLAYCARD_EVENT:
             //something
+            PlayCardEvent* return_pce = dynamic_cast<PlayCardEvent*>(message.get());
+            get_playcard_msg(return_pce, client, current_game);
+            //calls game function handleClientCardEvent();
+            
         break;
         case MESSAGETYPE_CLIENT_ACTION_EVENT:
             //something
@@ -37,10 +41,31 @@ void handleMessage(std::unique_ptr<Message> message, ClientID client){
         break;
         case MESSAGETYPE_CLIENT_DISCONNECT_EVENT:
             std::cout << "CLIENT DISCONNECTED: "<< client << std::endl;
-            //if this happens the game should receibe the message and tell the cardmanager to burn the plazers cards
+            //if this happens the game should receive the message and tell the cardmanager to burn 
+            //the players cards
         break;
         default:
             std::cout << "messagetype not found" << std::endl;
         break;
     }
 }
+
+
+void get_playcard_msg(PlayCardEvent* ret_msg, ClientID client, Game* current_game){
+    //now we take the message and break it down into the corresponding components
+    std::vector<Card> vector_of_cards;
+    for(auto card : ret_msg->cards){
+        vector_of_cards.push_back(card);
+    }
+    int slot = ret_msg->slot;
+    //here we should call the Game function handleClientCardEvent()
+    
+}
+
+            // case MESSAGETYPE_TEST:
+            //     dynamic_cast<TestMessage*>(m.get())->x = id;
+            //     for(auto client : clients) {
+            //         Network::sendMessage(m, client);
+            //         std::cout << "message sending with type: " << m->messageType << std::endl;
+            //     }
+            // break;

1368f42ff5eca5fe60b817abf51c34b7e37af87b
damcha02 <danil.poluyanov13@gmail.com>
2025-04-20 23:16:09 +0200

    some ideas on the constructor and implementation of class Game

---
+++ Server/game.cpp
@@ -0,0 +1,14 @@
+#include "game.hpp"
+
+//ctor
+Game::Game(Player player_count, Instance* parent_instance_m){
+    // needs to create a cardmanager, which will be alive for the whole game
+
+    // has to find first attacker -> either lowest trump or next player in order from prev durak
+
+    // needs to give out the roles 
+
+    // needs to call main game function
+
+}
+
--- Server/game.hpp
+++ Server/game.hpp
@@ -6,17 +6,23 @@ class Instance;
 
 class Game {
     private:
-    Instance* parent_instance;
+        Instance* parent_instance_m;
+        std::map<unsigned, Protocol::PlayerRole> player_roles_m;
 
     public:
-    //wenn player action nöd guet isch den eifach es eigenes mache nöd protocol modifiziere
-    typedef Protocol::GameAction PlayerAction;
-    typedef Protocol::Card Card;
-    typedef uint Player;
+        //wenn player action nöd guet isch den eifach es eigenes mache nöd protocol modifiziere
+        typedef Protocol::GameAction PlayerAction;
+        typedef Protocol::Card Card;
+        typedef uint Player;
 
-    //die signatures chasch au mache wie du willsch
-    Game(Player player_count, Instance* parent_instance);
-    void playerActionNotify(Player player, PlayerAction action);
-    void playerCardNotify(Player player, uint slot, std::list<Card> card);
-    void disconnectNotify(Player player); //unexpected disconnect mid game;
+        //die signatures chasch au mache wie du willsch
+        Game(Player player_count, Instance* parent_instance_m);
+        void playerActionNotify(Player player, PlayerAction action);
+        void playerCardNotify(Player player, uint slot, std::list<Card> card);
+        void disconnectNotify(Player player); //unexpected disconnect mid game;
+        
+        // whole game happens here
+        void mainGameLoop();
+
+        void findFirstAttacker();
 };

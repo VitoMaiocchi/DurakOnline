f008b83370d7f465c5b85c8d87348ab6c875dc3b
damcha02 <danil.poluyanov13@gmail.com>
2025-04-21 15:19:51 +0200

    card manager constructor functions

--- Server/card_manager.hpp
+++ Server/card_manager.hpp
@@ -5,13 +5,20 @@
 
 #include <vector>
 #include <deque>
+#include <algorithm>
+#include <cassert>
+#include <random>
+
 class CardManager{
     private:
         GameLogic::Suit trump_suit_m;
         GameLogic::Card trump_card_m = 0; //default value
 
         std::deque<GameLogic::Card> deck_m;
+        std::map<GameLogic::Player, std::vector<GameLogic::Card>> player_hands_m;
 
+        std::shared_ptr<GameLogic::Card> last_card_m = std::make_shared<GameLogic::Card>();
+        
     public:
         
         CardManager(GameLogic::Player player_count);
@@ -20,5 +27,6 @@ class CardManager{
         void shuffleCards();
         void fillDeck();
 
+        void distributeCardsBeginOfGame();
         void distributeNewCards();
 };

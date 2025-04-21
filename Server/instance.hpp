4403fb0494acad69e7c15275b7da7c410f06662b
damcha02 <danil.poluyanov13@gmail.com>
2025-04-21 14:48:45 +0200

    adapted to the gamelogic namspace

--- Server/instance.hpp
+++ Server/instance.hpp
@@ -9,12 +9,12 @@ class Instance {
     //das chasch du alles modifiziere. mach das wi du willsch
     void broadcastState(Protocol::GameStage stage,
         uint draw_pile_cards,
-        Game::Card trump_card, //wenn draw pile cards 0 isch rendered eifach nur de suit
-        std::map<Protocol::CardSlot, Game::Card> middle_cards,
-        std::map<Game::Player, uint> player_card_count,
-        std::map<Game::Player, Protocol::PlayerRole> player_roles);
+        GameLogic::Card trump_card, //wenn draw pile cards 0 isch rendered eifach nur de suit
+        std::map<Protocol::CardSlot, GameLogic::Card> middle_cards,
+        std::map<GameLogic::Player, uint> player_card_count,
+        std::map<GameLogic::Player, Protocol::PlayerRole> player_roles);
 
-    void sendPrivateState(Game::Player player,     
+    void sendPrivateState(GameLogic::Player player,     
         std::list<Protocol::GameAction> available_actions,
         std::list<Protocol::Card> cards);
 

04eb7488c8f45c8375cacdbb89dc4040c9f890c7
damcha02 <danil.poluyanov13@gmail.com>
2025-04-22 18:05:24 +0200

    assign all roles

--- Server/game.cpp
+++ Server/game.cpp
@@ -4,16 +4,24 @@
 
 using namespace GameLogic;
 
-#define Prole game_state_m.player_roles
+#define Proles game_state_m.player_roles
 #define Phand game_state_m.player_hands
 #define Pcount game_state_m.player_count
 #define Tcard game_state_m.trump_card
 
+int assignNextRole(int idx, Protocol::PlayerRole newrole, State &game_state_m){
+    int next = (idx + 1) % Pcount; 
+    Proles[next] = newrole;
+    return next;
+}
+
+//finds the first attacker based on the lowest trump, then sets all the other roles
 void findFirstAttacker(State &game_state_m){
+    using namespace Protocol;
     auto isTrump = [&game_state_m](const Card& c){
         return c.suit == game_state_m.trump_card.suit;
     };
-    Rank lowest = Protocol::Rank::RANK_ACE; //lowest trump card
+    Rank lowest = Rank::RANK_ACE; //lowest trump card
     int first_attacker = -1;
 
     for(Player i = 0; i < Pcount; ++i){
@@ -26,17 +34,25 @@ void findFirstAttacker(State &game_state_m){
             }
         }
     }
-    if(first_attacker != -1){
-        Prole[first_attacker] = Protocol::PlayerRole::ATTACKER;
-    }
+    if(first_attacker != -1){ //set all the player roles
+        int idx = first_attacker;
+        Proles[idx] = PlayerRole::ATTACKER;
 
+        idx = assignNextRole(idx, PlayerRole::DEFENDER, game_state_m);
+        idx = assignNextRole(idx, PlayerRole::CO_ATTACKER, game_state_m);
+        
+        int idleCount = Pcount - 3;
+        for(int i = 0; i < idleCount; ++i){
+            idx = assignNextRole(idx, PlayerRole::IDLE, game_state_m);
+        }
+    }
 }
 
 //ctor
 Game::Game(Player player_count, Instance* parent_instance_m) : game_state_m(player_count){
 
     
-    Prole.reserve(player_count); //preallocate enough space
+    Proles.reserve(player_count); //preallocate enough space
 
     findFirstAttacker(game_state_m); //find first attacker
 

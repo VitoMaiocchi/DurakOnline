701aeafff9b9ae34a73848c229ccc46daff08e64
damcha02 <danil.poluyanov13@gmail.com>
2025-04-28 00:01:58 +0200

    declared helpers to test them later

--- Server/helperheader.hpp
+++ Server/helperheader.hpp
@@ -17,13 +17,23 @@ void clearMiddle(State &state);
 void drawFromMiddle(Player player, State &state);
 void distributeNewCards(State &state);
 
+//doneEvent Helpers and more
 // role/hand removal
-std::vector<Player> findFinishedPlayers(State &state);
 void movePlayerRoles(State &state);
-void eraseFromRolesAndHands(Player player_idx, State &state);
+void movePlayerRolesOneBack(State &state);
+std::vector<Player> findFinishedPlayers(State &state);
+void setPlayerToFinished(Player player_idx, State &state);
 void eraseFinishedPlayer(Player player_idx, State &state);
+bool onlyOnePlayerLeft(State &state);
+Player findLastPlayer(State &state);
 void removeFinishedPlayers(State &state);
 
 // battle flow
 void deleteOldBattle(State &state);
 void startNewBattle(State &state);
+
+//reflect Event helpers And more
+bool topSlotsClear(State &state);
+bool nextPlayerHasEnoughCards(Player next_player, State &state);
+bool ranksMatchToPassOn(Protocol::Rank rank, State &state);
+std::optional<Card> getReflectCard(Player player, State &state);
